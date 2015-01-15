d044a271ac26249120ff56eca24f8ff1a317de58
maierb <maier@maierb.com>
2015-01-15 13:57:15 -0500

    Added Project Files

---
+++ app/code/local/Skyglue/Customertab/Block/Adminhtml/Customer/Edit/Tab/Userreport.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * Adminhtml customer action tab
+ *
+ */
+class Skyglue_Customertab_Block_Adminhtml_Customer_Edit_Tab_Userreport
+    extends Mage_Adminhtml_Block_Template
+    implements Mage_Adminhtml_Block_Widget_Tab_Interface
+{
+
+    public function __construct()
+    {
+        $this->setTemplate('skyglue/userreport.phtml');
+
+    }
+
+    /**
+     * Defines after which tab, this tab should be rendered
+     *
+     * @return string
+     */
+    public function getAfter()
+    {
+        return 'orders';
+    }
+
+    /**
+     * Return Tab label
+     *
+     * @return string
+     */
+    public function getTabLabel()
+    {
+        $label = '<span style="background:url(/skin/adminhtml/default/default/skyglue/skyglue.png) no-repeat left 1px; padding:0 0 0 20px">Skyglue User Report';
+        return ($label);
+    }
+
+    /**
+     * Return Tab title
+     *
+     * @return string
+     */
+    public function getTabTitle()
+    {
+        return $this->__('Skyglue User Report');
+    }
+
+    /**
+     * Can show tab in tabs
+     *
+     * @return boolean
+     */
+    public function canShowTab()
+    {
+        $customer = Mage::registry('current_customer');
+        return (bool)$customer->getId();
+    }
+
+    /**
+     * Tab is hidden
+     *
+     * @return boolean
+     */
+    public function isHidden()
+    {
+        return false;
+    }
+
+    /**
+     * AJAX TAB's
+     * If you want to use an AJAX tab, uncomment the following functions
+     * Please note that you will need to setup a controller to recieve
+     * the tab content request
+     *
+     */
+    /**
+     * Retrieve the class name of the tab
+     * Return 'ajax' here if you want the tab to be loaded via Ajax
+     *
+     * return string
+     */
+ #  public function getTabClass()
+ #  {
+ #      return 'ajax';
+ #  }
+
+    /**
+     * Determine whether to generate content on load or via AJAX
+     * If true, the tab's content won't be loaded until the tab is clicked
+     * You will need to setup a controller to handle the tab request
+     *
+     * @return bool
+     */
+#   public function getSkipGenerateContent()
+#   {
+#       return true;
+#   }
+
+    /**
+     * Retrieve the URL used to load the tab content
+     * Return the URL here used to load the content by Ajax
+     * see self::getSkipGenerateContent & self::getTabClass
+     *
+     * @return string
+     */
+#   public function getTabUrl()
+#   {
+ #      return $this->getUrl('*/*/skyglue', array('_current' => true));
+ #  }
+
+}
+?>
